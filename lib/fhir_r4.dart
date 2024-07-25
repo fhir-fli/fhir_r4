311cb248d82f367bb4f8ce90e5b57d33a2118e91
Dokotela <grey.faulkenberry@mayjuun.com>
2024-07-24 20:39:15 -0400

    should have removed these already

--- lib/fhir_r4.dart
+++ lib/fhir_r4.dart
@@ -2,7 +2,6 @@ export 'package:fhir_primitives/fhir_primitives.dart';
 export 'src/fhir/r4.dart';
 export 'src/fhir_at_rest/r4.dart';
 export 'src/fhir_bulk/r4.dart';
-export 'src/fhir_db/fhir_drift.dart';
 export 'src/fhir_db/r4.dart';
 export 'src/fhir_path/r4.dart';
 export 'src/fhir_yaml/fhir_yaml.dart';
