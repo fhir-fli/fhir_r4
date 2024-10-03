9e508c5eb834ffecc0018adf2fc764813277dc21
Dokotela <grey.faulkenberry@mayjuun.com>
2024-10-03 19:26:44 -0400

    here goes nothing

--- generate/generate.dart
+++ generate/generate.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: avoid_dynamic_calls
+
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:archive/archive_io.dart';
@@ -6,10 +9,91 @@ Future<void> main() async {
   await extractFilesToDisk();
   await moveJsonExamples();
   await moveNdJsonExamples();
+  await actualFHIRClasses();
 
   // File('definitions.json.zip').deleteSync();
   // File('examples-json.zip').deleteSync();
   // File('examples-ndjson.zip').deleteSync();
+}
+
+Future<void> actualFHIRClasses() async {
+  await extractFileToDisk('definitions.json/fhir.schema.json.zip', '.');
+
+  final String schemaString = await File('fhir.schema.json').readAsString();
+  final Map<String, dynamic> schema =
+      jsonDecode(schemaString) as Map<String, dynamic>;
+
+  // Load the schema definitions
+  final Map<String, dynamic> definitions =
+      schema['definitions'] as Map<String, dynamic>;
+
+  // Iterate over all types and generate Dart classes
+  for (final String key in definitions.keys) {
+    final Map<String, dynamic> classDefinition =
+        definitions[key] as Map<String, dynamic>;
+
+    // Generate Dart code for the class
+    final String dartClass = generateDartClass(key, classDefinition);
+
+    // Write the Dart class to a file
+    final String outputPath =
+        'lib/fhir/$key.dart'; // Organize paths based on categories
+    await File(outputPath).writeAsString(dartClass);
+  }
+}
+
+// Function to generate Dart class from schema definition
+String generateDartClass(
+    String className, Map<String, dynamic> classDefinition) {
+  final StringBuffer buffer = StringBuffer();
+
+  // Add imports
+  buffer.writeln("import 'package:json_annotation/json_annotation.dart';");
+  buffer.writeln("import 'package:equatable/equatable.dart';");
+
+  // Class declaration
+  buffer.writeln('@JsonSerializable()');
+  buffer.writeln('class $className extends Equatable {');
+
+  // Fields
+  final Map<String, dynamic> properties =
+      classDefinition['properties'] as Map<String, dynamic>;
+  properties.forEach((String field, dynamic details) {
+    final String type = details['type'] as String? ?? 'dynamic';
+    final bool isRequired =
+        (classDefinition['required'] as List<dynamic>?)?.contains(field) ??
+            false;
+    buffer.writeln('  final ${isRequired ? '' : ''}$type $field;');
+  });
+
+  // Constructor
+  buffer.writeln('  const $className({');
+  properties.forEach((String field, dynamic details) {
+    final bool isRequired =
+        (classDefinition['required'] as List<dynamic>?)?.contains(field) ??
+            false;
+    buffer.writeln('    ${isRequired ? 'required this.' : 'this.'}$field,');
+  });
+  buffer.writeln('  });');
+
+  // Add toJson/fromJson methods
+  buffer.writeln(
+      'factory $className.fromJson(Map<String, dynamic> json) => _\$${className}FromJson(json);');
+  buffer
+      .writeln('Map<String, dynamic> toJson() => _\$${className}ToJson(this);');
+
+  // Add Equatable properties (for equality and hashCode)
+  buffer.writeln('@override');
+  buffer.writeln('List<Object?> get props => [');
+  properties.forEach((String field, _) {
+    buffer.writeln('    $field,');
+  });
+  buffer.writeln('  ];');
+
+  // Close class
+  buffer.writeln('}');
+
+  return buffer.toString();
 }
 
 Future<void> moveNdJsonExamples() async {
