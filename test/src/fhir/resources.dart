7276de24bfbcd49197c08da7653fc51f0a5845ed
Dokotela <grey.faulkenberry@mayjuun.com>
2024-11-09 10:15:17 -0800

    still debugging

--- test/src/fhir/resources.dart
+++ test/src/fhir/resources.dart
@@ -33,11 +33,11 @@ Future<List<String>> r4Validation() async {
   final dir = Directory('./test/src/fhir/assets');
   final string = <String>[];
   for (final file in dir.listSync()) {
+    print(file.path);
     final contents = File(file.path).readAsStringSync();
+    final contentJson = jsonDecode(contents) as Map<String, dynamic>;
+    final resource = Resource.fromJson(contentJson);
     try {
-      final contentJson = jsonDecode(contents) as Map<String, dynamic>;
-      final resource = Resource.fromJson(contentJson);
-
       if (!_compare(
         contentJson,
         resource.toJson(),
@@ -49,6 +49,11 @@ Future<List<String>> r4Validation() async {
         throw Exception('Unequal');
       }
     } catch (e) {
+      File(file.path.replaceAll('assets/', '').replaceAll('.json', '1.json'))
+          .writeAsStringSync(prettyPrintJson(contentJson));
+      File(file.path.replaceAll('assets/', '').replaceAll('.json', '2.json'))
+          .writeAsStringSync(prettyPrintJson(resource.toJson()));
+
       print(file.path);
       rethrow;
     }
@@ -56,120 +61,75 @@ Future<List<String>> r4Validation() async {
   return string;
 }
 
-bool _compare(dynamic left, dynamic right) {
-  if (left is Map && right is Map) {
-    // Check if both Maps have the same keys
-    if (left.keys.toSet() != right.keys.toSet()) {
-      return false;
-    }
-    // Recursively compare each key-value pair
-    for (final key in left.keys) {
-      if (!_compare(left[key], right[key])) {
-        return false;
-      }
-    }
-    return true;
-  } else if (left is Iterable && right is Iterable) {
-    // Ensure both Iterables have the same content
-    final leftList = left.toList();
-    final rightList = right.toList();
-
-    // Check if they have the same length
-    if (leftList.length != rightList.length) {
-      return false;
-    }
-
-    // Sort both lists for unordered comparison, then compare each element
-    leftList.sort((a, b) => a.hashCode.compareTo(b.hashCode));
-    rightList.sort((a, b) => a.hashCode.compareTo(b.hashCode));
-
-    for (var i = 0; i < leftList.length; i++) {
-      if (!_compare(leftList[i], rightList[i])) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    // Compare other values by JSON serialization for deep comparison
-    return jsonEncode(left) == jsonEncode(right);
+dynamic decodeUrlIfString(dynamic value) {
+  // Decode the value only if it's a string and a URL with encoded characters
+  if (value is String) {
+    return Uri.decodeFull(value);
   }
+  return value;
 }
 
-// bool _compare(dynamic left, dynamic right) {
-//   if (left is Map && right is Map) {
-//     if (left.keys.length != right.keys.length) {
-//       // print('keys different length');
-//       return false;
-//     }
-//     final leftKeys = left.keys.toList();
-//     final rightKeys = right.keys.toList();
-//     for (final key in leftKeys) {
-//       if (!rightKeys.contains(key)) {
-//         // print('right does not contain key $key');
-//         return false;
-//       } else {
-//         rightKeys.remove(key);
-//       }
-//     }
-//     if (rightKeys.isNotEmpty) {
-//       // print('right contains keys not in left: $rightKeys');
-//       return false;
-//     }
-//     for (final key in left.keys) {
-//       if (!_compare(left[key], right[key])) {
-//         // print('Value for key $key is unequal');
-//         return false;
-//       }
-//     }
-//     return true;
-//   } else if (left is Iterable && right is Iterable) {
-//     if (left.any((e) => e is Map) || right.any((e) => e is Map)) {
-//       if (left.any((e) => e is! Map) || right.any((e) => e is! Map)) {
-//         // print('There are some objects that are Maps and others are not');
-//         return false;
-//       }
-//     }
-//     if (left.any((e) => e is Iterable) || right.any((e) => e is Iterable)) {
-//       if (left.any((e) => e is! Iterable) || right.any((e) => e is! Iterable)) {
-//         // print('There are some entries that are Iterables, others are not');
-//         return false;
-//       }
-//     }
-
-//     final tempRight = right.toList();
-//     for (final leftEntry in left) {
-//       var found = false;
-//       for (final rightEntry in right) {
-//         if (_compare(leftEntry, rightEntry)) {
-//           tempRight.remove(rightEntry);
-//           found = true;
-//           break;
-//         }
-//       }
-//       if(!found){
-//         // print('error in list');
-//         return false;
-//       }
-//     }
-//     return true;
-//   } else {
-//     final result =
-//         jsonDecode(jsonEncode(left)) == jsonDecode(jsonEncode(right));
-//     if(!result){
-//       print('left: $left != right $right');
-//     }
-//     return result;
-//   }
-// }
+bool _compare(dynamic oldLeft, dynamic oldRight) {
+  const equality = DeepCollectionEquality.unordered(CustomBaseEquality());
+  return equality.equals(
+    oldLeft,
+    oldRight,
+  );
+}
 
 /// Returns a pretty printed JSON string.
 const JsonEncoder jsonEncoder = JsonEncoder.withIndent('    ');
 
 /// Returns a pretty printed JSON string.
-String jsonPrettyPrint(Map<String, dynamic> map) => jsonEncoder.convert(map);
-
-/// Returns a pretty printed JSON string.
 String prettyPrintJson(Map<String, dynamic> map) => jsonEncoder.convert(map);
 
-/// Returns a pretty printed JSON string.
-String prettyPrintAnything(dynamic anything) => jsonEncoder.convert(anything);
+class CustomBaseEquality extends DefaultEquality<Object?> {
+  const CustomBaseEquality();
+
+  @override
+  bool equals(Object? o1, Object? o2) {
+    if (o1 is String && o2 is String) {
+      return const CustomStringEquality().equals(o1, o2);
+    }
+    return super.equals(o1, o2);
+  }
+
+  @override
+  int hash(Object? o) {
+    if (o is String) {
+      return const CustomStringEquality().hash(o);
+    }
+    return super.hash(o);
+  }
+}
+
+class CustomStringEquality implements Equality<String> {
+  const CustomStringEquality();
+
+  @override
+  bool equals(String str1, String str2) {
+    try {
+      // Decode URLs if needed before comparison
+      return Uri.decodeFull(str1) == Uri.decodeFull(str2);
+    } catch (e) {
+      // If decoding fails, fall back to direct comparison and log the error
+      print('Invalid URL encoding detected: "$str1" or "$str2". Error: $e');
+      return str1 == str2;
+    }
+  }
+
+  @override
+  int hash(String str) {
+    try {
+      // Use decoded value for hash calculation
+      return Uri.decodeFull(str).hashCode;
+    } catch (e) {
+      // If decoding fails, log the error and return hash of original string
+      print('Invalid URL encoding for hash calculation: "$str". Error: $e');
+      return str.hashCode;
+    }
+  }
+
+  @override
+  bool isValidKey(Object? o) => o is String;
+}
