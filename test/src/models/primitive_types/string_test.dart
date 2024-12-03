0809d21cb3716d6e486371294c0af765d8c1edce
Dokotela <grey.faulkenberry@mayjuun.com>
2024-12-03 14:43:52 -0500

    move test files

---
+++ test/src/models/primitive_types/string_test.dart
@@ -0,0 +1,138 @@
+import 'package:fhir_r4/fhir_r4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('FhirString Tests', () {
+    const validString = 'Hello, FHIR!';
+    const otherString = ' How are you?';
+    const jsonString = 'JSON String';
+    const yamlString = 'YAML String';
+    const emptyString = '';
+    final element = Element(id: 'testElement'.toFhirString);
+
+    // Basic FhirString creation and validation
+    test('FhirString from String value', () {
+      final fhirString = FhirString(validString);
+      expect(fhirString.value, equals(validString));
+      expect(fhirString.toString(), equals(validString));
+      expect(fhirString.toJson()['value'], equals(validString));
+    });
+
+    test('FhirString fromJson with valid String', () {
+      final fhirString = FhirString.fromJson({'value': jsonString});
+      expect(fhirString.value, equals(jsonString));
+      expect(fhirString.toJson()['value'], equals(jsonString));
+    });
+
+    test('FhirString fromJson with invalid input throws FormatException', () {
+      expect(
+        () => FhirString.fromJson({'value': 123}),
+        throwsA(isA<TypeError>()),
+      );
+    });
+
+    test('FhirString fromYaml with valid YAML', () {
+      final fhirString = FhirString.fromYaml('value: $yamlString');
+      expect(fhirString.value, equals(yamlString));
+      expect(fhirString.toJson()['value'], equals(yamlString));
+    });
+
+    test('FhirString tryParse with valid String', () {
+      final fhirString = FhirString.tryParse(validString);
+      expect(fhirString?.value, equals(validString));
+    });
+
+    test('FhirString tryParse with invalid input returns null', () {
+      final fhirString =
+          FhirString.tryParse(123); // Not a valid input for FhirString
+      expect(fhirString, isNull);
+    });
+
+    test('FhirString tryParse with empty string', () {
+      final fhirString = FhirString.tryParse(emptyString);
+      expect(fhirString?.value, equals(emptyString));
+    });
+
+    test('FhirString equality with another FhirString', () {
+      final fhirString1 = FhirString(validString);
+      final fhirString2 = FhirString(validString);
+      expect(fhirString1 == fhirString2, isTrue);
+      expect(fhirString1.equals(fhirString2), isTrue);
+    });
+
+    test('FhirString equality with a String', () {
+      final fhirString = FhirString(validString);
+      // ignore: unrelated_type_equality_checks
+      expect(fhirString == validString, isTrue);
+    });
+
+    test('FhirString inequality with a different String', () {
+      final fhirString = FhirString(validString);
+      // ignore: unrelated_type_equality_checks
+      expect(fhirString == 'Different String', isFalse);
+    });
+
+    // Additional string-specific methods
+    test('FhirString length', () {
+      final fhirString = FhirString(validString);
+      expect(fhirString.length, equals(validString.length));
+    });
+
+    test('FhirString isEmptyString', () {
+      final fhirStringEmpty = FhirString(emptyString);
+      final fhirStringNonEmpty = FhirString(validString);
+      expect(fhirStringEmpty.isEmptyString, isTrue);
+      expect(fhirStringNonEmpty.isEmptyString, isFalse);
+    });
+
+    test('FhirString isNotEmpty', () {
+      final fhirStringEmpty = FhirString(emptyString);
+      final fhirStringNonEmpty = FhirString(validString);
+      expect(fhirStringEmpty.isNotEmpty, isFalse);
+      expect(fhirStringNonEmpty.isNotEmpty, isTrue);
+    });
+
+    test('FhirString addition (concatenation)', () {
+      final fhirString = FhirString(validString);
+      final concatenated = fhirString + otherString;
+      expect(concatenated, equals(validString + otherString));
+    });
+
+    test('FhirString substring', () {
+      final fhirString = FhirString(validString);
+      final substring = fhirString.substring(7);
+      expect(substring, equals(validString.substring(7)));
+    });
+
+    test('FhirString trim', () {
+      final fhirString = FhirString('   $validString   ');
+      expect(fhirString.trim(), equals(validString));
+    });
+
+    // FhirString-specific methods inherited from PrimitiveType
+    test('FhirString clone', () {
+      final originalString = FhirString(validString);
+      final clonedString = originalString.clone();
+      expect(clonedString.value, equals(validString));
+      expect(clonedString == originalString, isTrue);
+      expect(clonedString.hashCode, equals(originalString.hashCode));
+    });
+
+    test('FhirString copyWith new value', () {
+      final originalString = FhirString(validString);
+      final copiedString = originalString.copyWith(newValue: 'New String');
+      expect(copiedString.value, equals('New String'));
+      expect(originalString.value, equals(validString));
+    });
+
+    test('FhirString with Element', () {
+      final fhirString = FhirString(validString, element);
+      expect(fhirString.element?.id, equals('testElement'.toFhirString));
+    });
+
+    test('FhirString toJsonString', () {
+      final fhirString = FhirString(validString);
+      expect(fhirString.toJsonString(), equals('{"value":"$validString"}'));
+    });
+  });
+}
---
+++ test/src/models/resources_test.dart
@@ -0,0 +1,139 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+
+import 'package:collection/collection.dart';
+import 'package:fhir_r4/fhir_r4.dart';
+import 'package:test/test.dart';
+import 'package:universal_io/io.dart';
+
+void main() {
+  group(
+    'Validation',
+    () {
+      test(
+        '\n****R4 JSON is being Validated****',
+        () {
+          final tested = <String>[...r4Validation()];
+          expect(tested.isEmpty, true);
+        },
+        timeout: const Timeout(Duration(minutes: 15)),
+      );
+    },
+  );
+}
+
+String filePath = '';
+
+List<String> r4Validation() {
+  final dir = Directory('./test/src/models/assets');
+  final string = <String>[];
+  for (final file in dir.listSync()) {
+    print(file.path);
+    filePath = file.path;
+    final contents = File(file.path).readAsStringSync();
+    final contentJson = jsonDecode(contents) as Map<String, dynamic>;
+    final resource = Resource.fromJson(contentJson);
+    try {
+      if (!const DeepCollectionEquality()
+          .equals(contentJson, resource.toJson())) {
+        if (!_deepCompare(
+          contentJson,
+          resource.toJson(),
+        )) {
+          File(
+            file.path.replaceAll('assets/', '').replaceAll(
+                  '.json',
+                  '1.json',
+                ),
+          ).writeAsStringSync(prettyPrintJson(contentJson));
+          File(
+            file.path.replaceAll('assets/', '').replaceAll(
+                  '.json',
+                  '2.json',
+                ),
+          ).writeAsStringSync(prettyPrintJson(resource.toJson()));
+          throw Exception('Unequal');
+        }
+      }
+    } catch (e) {
+      File(file.path.replaceAll('assets/', '').replaceAll('.json', '1.json'))
+          .writeAsStringSync(prettyPrintJson(contentJson));
+      File(file.path.replaceAll('assets/', '').replaceAll('.json', '2.json'))
+          .writeAsStringSync(prettyPrintJson(resource.toJson()));
+
+      print(file.path);
+      rethrow;
+    }
+  }
+  return string;
+}
+
+bool _deepCompare(dynamic json1, dynamic json2) {
+  return const DeepCollectionEquality.unordered(CustomBaseEquality())
+      .equals(_normalizeJson(json1), _normalizeJson(json2));
+}
+
+dynamic _normalizeJson(dynamic json) {
+  if (json is List) {
+    return json.map(_normalizeJson).toList()..sort(_listSort);
+  } else if (json is Map) {
+    return json.map((key, value) => MapEntry(key, _normalizeJson(value)));
+  }
+  return json;
+}
+
+int _listSort(dynamic a, dynamic b) {
+  final strA = jsonEncode(a);
+  final strB = jsonEncode(b);
+  return strA.compareTo(strB);
+}
+
+const JsonEncoder jsonEncoder = JsonEncoder.withIndent('    ');
+
+String prettyPrintJson(Map<String, dynamic> map) => jsonEncoder.convert(map);
+
+class CustomBaseEquality extends DefaultEquality<Object?> {
+  const CustomBaseEquality();
+
+  @override
+  bool equals(Object? o1, Object? o2) {
+    if (o1 is String && o2 is String) {
+      return const CustomStringEquality().equals(o1, o2);
+    }
+    return super.equals(o1, o2);
+  }
+
+  @override
+  int hash(Object? o) {
+    if (o is String) {
+      return const CustomStringEquality().hash(o);
+    }
+    return super.hash(o);
+  }
+}
+
+class CustomStringEquality implements Equality<String> {
+  const CustomStringEquality();
+
+  @override
+  bool equals(String str1, String str2) {
+    try {
+      return Uri.decodeFull(str1) == Uri.decodeFull(str2);
+    } catch (e) {
+      return str1 == str2;
+    }
+  }
+
+  @override
+  int hash(String str) {
+    try {
+      return Uri.decodeFull(str).hashCode;
+    } catch (e) {
+      return str.hashCode;
+    }
+  }
+
+  @override
+  bool isValidKey(Object? o) => o is String;
+}
