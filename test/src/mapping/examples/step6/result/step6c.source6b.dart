b0591ff4f64dc60b5ba530ed791319f32e8606d3
Dokotela <grey.faulkenberry@mayjuun.com>
2025-03-11 13:56:06 -0400

    Much smaller now

--- test/src/mapping/examples/step6/result/step6c.source6b.dart
+++
@@ -1,4 +0,0 @@
-// ignore_for_file: prefer_single_quotes, always_specify_types,
-// ignore_for_file: avoid_escaping_inner_quotes
-
-final resultStep6cSource6b = {"resourceType": "TRight", "a23": 67890};
