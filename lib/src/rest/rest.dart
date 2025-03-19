381c364e54eca45b8aa954de56d94c76f1bbee41
Dokotela <grey.faulkenberry@mayjuun.com>
2025-03-19 19:29:43 -0400

    fhir_r4_at_rest setup

--- lib/src/rest/rest.dart
+++
@@ -1,4 +0,0 @@
-export 'enums/enums.dart';
-export 'fhir_request.dart';
-export 'searches/searches.dart';
-export 'utils/utils.dart';
--- lib/src/rest/utils/utils.dart
+++
@@ -1,4 +0,0 @@
-export 'parse_request_result.dart';
-export 'patch_body.dart';
-export 'restful_parameters.dart';
-export 'return_results.dart';
