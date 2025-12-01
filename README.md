99daf37d6209cc9d7d2b1af30a7dadf9b943ea6b
Dokotela <Dokotela@users.noreply.github.com>
2025-12-01 12:16:25 -0500

    don't want to save keys

--- README.md
+++ README.md
@@ -35,3 +35,4 @@ R4b Version of the FHIR Specification
 
 A Dart/Flutter package for working with FHIR® resources. FHIR® is the registered trademark of HL7 and is used with the permission of HL7. Use of the FHIR trademark does not constitute endorsement of this product by HL7.
 
+https://github.com/WorldHealthOrganization/smart-immunizations/blob/main/input/maps/IMMZ.C4.QRToPatient.fml
---
+++ packages/fhir_r4_auth/test/check_url.dart
@@ -0,0 +1,15 @@
+import 'package:fhir_r4/fhir_r4.dart';
+import 'package:fhir_r4_auth/fhir_r4_auth.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../test_helpers/test_data.dart';
+
+void main() {
+  test('Check URL', () {
+    print('Epic base URL: ${TestServers.epicBaseUrl}');
+    final uri = TestServers.epicBaseUrl.toFhirUri;
+    print('FhirUri: $uri');
+    print('Metadata URL: ${uri}/metadata');
+    final parsed = Uri.parse('${uri}/metadata');
+    print('Parsed: $parsed');
+  });
+}
