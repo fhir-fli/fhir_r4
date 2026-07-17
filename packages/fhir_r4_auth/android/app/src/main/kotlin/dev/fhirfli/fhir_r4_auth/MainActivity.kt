5eb8b4f20c33db0e1c59ed58d01ff7cab3909cf8
Dokotela <jgfaulkenberry@gmail.com>
2026-07-17 12:11:59 -0400

    Publish strip for 0.6.0: remove tests and auth demo/platform dirs from main
    main is the publish branch: package test/ trees (including the large
    test asset corpora) and the fhir_r4_auth demo apps + platform
    directories live on dev only. Examples ship.

--- packages/fhir_r4_auth/android/app/src/main/kotlin/dev/fhirfli/fhir_r4_auth/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package dev.fhirfli.fhir_r4_auth
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- packages/fhir_r4_auth/ehr_launch_demo/android/app/src/main/kotlin/com/example/ehr_launch_demo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ehr_launch_demo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
